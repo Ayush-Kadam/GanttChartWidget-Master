e862001da4a6af1e8840be763f10f8db2557ac04
Ayush-Kadam <141824669+Ayush-Kadam@users.noreply.github.com>
2023-10-12 11:56:18 +0530

    GanttChart-Master

---
+++ src/ide/index.ts
@@ -0,0 +1,220 @@
+// automatically import the css file
+import './ide.css';
+import {
+    TWWidgetDefinition,
+    autoResizable,
+    description,
+    property,
+    defaultValue,
+    bindingTarget,
+    service,
+    event,
+    bindingSource,
+    nonEditable,
+    willSet,
+    didSet,
+    displayName,
+    TWNamedComposerWidget,
+} from 'typescriptwebpacksupport/widgetIDESupport';
+
+
+
+
+@description('A widget')
+@TWNamedComposerWidget("ganttChart")
+class GanttChartWidget extends TWComposerWidget {
+
+    widgetIconUrl(): string {
+        return require('../images/icon.svg');
+    }
+    
+    
+    widgetProperties(): TWWidgetProperties {
+        require("./ide.css");
+        return {
+          'name': 'Gantt Chart',
+          'description': 'Displays a gantt chart of tasks and related durations',
+          'category': ['Data'],
+          'defaultBindingTargetProperty': 'Data',
+          'supportsAutoResize': true,
+          'properties': {
+              'TaskName': {
+                  'description': 'Field to use for task name',
+                  'baseType': 'FIELDNAME',
+                  'sourcePropertyName': 'Data'
+              },
+              'TaskID': {
+                  'description': 'Field to use for task ID',
+                  'baseType': 'FIELDNAME',
+                  'sourcePropertyName': 'Data'
+              },
+              'StartDate': {
+                  'description': 'Field to use for start date',
+                  'baseType': 'FIELDNAME',
+                  'sourcePropertyName': 'Data'
+              },
+              'EndDate': {
+                  'description': 'Field to use for end date',
+                  'baseType': 'FIELDNAME',
+                  'sourcePropertyName': 'Data'
+              },
+              'Completed': {
+                  'description': 'Field to use to indicate if task is completed',
+                  'baseType': 'FIELDNAME',
+                  'sourcePropertyName': 'Data'
+              },
+              'Relationships': {
+                  'description': 'Field to use to indicate relationships of tasks. Comma separated TaskIDs',
+                  'baseType': 'FIELDNAME',
+                  'sourcePropertyName': 'Data'
+              },
+              'CustomClass': {
+                  'description': 'Custom class to use for drawing this element',
+                  'baseType': 'FIELDNAME',
+                  'sourcePropertyName': 'Data'
+              },
+              'TooltipField1': {
+                  'description': 'Field which will provide 1st tooltip data',
+                  'isBindingTarget': true,
+                  'isBindingSource':true,
+                  'isVisible': true,
+                  'isEditable': true,
+                  'sourcePropertyName': 'Data',
+                  'baseType': 'FIELDNAME'
+              },
+              'TooltipField2': {
+                  'description': 'Field which will provide 2nd tooltip data',
+                  'isBindingTarget': true,
+                  'isBindingSource':true,
+                  'isVisible': true,
+                  'isEditable': true,
+                  'sourcePropertyName': 'Data',
+                  'baseType': 'FIELDNAME'
+              },
+              'TooltipField3': {
+                  'description': 'Field which will provide 3rd tooltip data',
+                  'isBindingTarget': true,
+                  'isBindingSource':true,
+                  'isVisible': true,
+                  'isEditable': true,
+                  'sourcePropertyName': 'Data',
+                  'baseType': 'FIELDNAME'
+              },
+              'Data': {
+                  'description': 'Data source',
+                  'isBindingTarget': true,
+                  'isBindingSource':true,
+                  'isEditable': true,
+                  'baseType': 'INFOTABLE',
+                  'warnIfNotBoundAsTarget': true
+              },
+              'ItemHeight': {
+                  'description': 'Field to use for the element height',
+                  'baseType': 'NUMBER',
+                  'defaultValue': 23,
+                  'isBindingTarget': false
+              },
+              'BarCornerRadius': {
+                  'description': 'Field to use for the bar corner radius',
+                  'baseType': 'NUMBER',
+                  'defaultValue': 2,
+                  'isBindingTarget': false
+              },
+              'ArrowRadius': {
+                  'description': 'Field to use for the arrow radius',
+                  'baseType': 'NUMBER',
+                  'defaultValue': 5,
+                  'isBindingTarget': false
+              },
+              'HeaderHeight': {
+                  'description': 'How tall the header should be',
+                  'baseType': 'NUMBER',
+                  'defaultValue': 50,
+                  'isBindingTarget': false
+              },
+              'ColumnWidth': {
+                  'description': 'How big each column should be (distance between two dates)',
+                  'baseType': 'NUMBER',
+                  'defaultValue': 30,
+                  'isBindingTarget': false
+              },
+              'ViewMode': {
+                  'description': 'Granularity of the minimum interval displayed',
+                  'baseType': 'STRING',
+                  'defaultValue': 'Day',
+                  'selectOptions': ['Quarter Day', 'Half Day', 'Day', 'Week', 'Month'].map((option) => {return {text: option, value: option}}),
+                  'isBindingTarget': true,
+                  'isBindingSource': true
+              },
+              'Padding': {
+                  'description': 'Space between eleemnts',
+                  'baseType': 'NUMBER',
+                  'defaultValue': 18,
+                  'isBindingTarget': false
+              },
+              'DateFormat': {
+                  'description': 'How to format the dates in the widget',
+                  'baseType': 'STRING',
+                  'defaultValue': 'YYYY-MM-DD',
+                  'isLocalizable': true
+              },
+              'AlwaysShowHandles': {
+                  'description': 'Always show the handles for tasks. Especially useful for showing tasks over limit',
+                  'baseType': 'BOOLEAN',
+                  'defaultValue': false
+              },
+              'ShowBaselines': {
+                  'description': 'For percentages > 100%, show the baseline underneath the values',
+                  'baseType': 'BOOLEAN',
+                  'defaultValue': false
+              }
+          }
+      }
+    };
+
+    widgetServices(): Dictionary<TWWidgetService> {
+        return {};
+    };
+
+    widgetEvents(): Dictionary<TWWidgetEvent> {
+        return {
+            'DoubleClicked': {
+                'warnIfNotBound': false
+            }
+        };
+    }
+
+    
+    renderHtml(): string {
+        return '<div class="widget-content widget-ganttChart"><table height="100%" width="100%"><tr><td valign="middle" align="center"><span>Gantt Chart</span></td></tr></table></div>';
+    };
+
+   
+
+    
+    validate(): { severity: string, message: string }[] {
+        var result = [];
+        if (this.isPropertyBoundAsTarget('Data')) {
+          if (this.getProperty('TaskID') === undefined || this.getProperty('TaskID').length === 0) {
+              result.push({ severity: 'warning', message: 'TaskID for {target-id} must be chosen' });
+          }
+          if (this.getProperty('TaskName') === undefined || this.getProperty('TaskName').length === 0) {
+              result.push({ severity: 'warning', message: 'TaskName for {target-id} must be chosen' });
+          }
+    if (this.getProperty('StartDate') === undefined || this.getProperty('StartDate').length === 0) {
+              result.push({ severity: 'warning', message: 'StartDate for {target-id} must be chosen' });
+          }
+    if (this.getProperty('EndDate') === undefined || this.getProperty('EndDate').length === 0) {
+              result.push({ severity: 'warning', message: 'EndDate for {target-id} must be chosen' });
+          }
+      } 
+        return result;
+    }
+
+    afterRender(): void {
+    }
+
+    beforeDestroy(): void {
+    }
+
+}
---
+++ src/runtime/index.ts
@@ -0,0 +1,187 @@
+import {
+    TWWidgetDefinition,
+    property,
+    canBind,
+    TWEvent,
+    event,
+    service,
+    TWProperty,
+    TWNamedRuntimeWidget,
+} from 'typescriptwebpacksupport/widgetRuntimeSupport';
+import Gantt from 'frappe-gantt';
+import widgetIconUrl from '../images/icon.svg';
+
+
+@TWNamedRuntimeWidget("ganttChart")
+class GanttChartWidget extends TWRuntimeWidget {
+    serviceInvoked() { };
+    currentGanttChart: any;
+    currentViewMode: string;
+    elementStyleSheet: CSSStyleSheet;
+    
+    @TWProperty("ViewMode")
+    set viewMode(value) {
+        if (value) {
+            this.currentViewMode = value;
+            this.currentGanttChart.change_view_mode(value);
+        }
+    }
+
+    renderHtml(): string {
+        require("./runtime.css");
+        let widgetHeight, widgetWidth;
+        if (this.getProperty('ResponsiveLayout')) {
+            widgetWidth = '100%';
+            widgetHeight = '100%';
+        } else {
+            widgetWidth = this.getProperty("Width");
+            widgetHeight = this.getProperty("Height");
+        }
+        this.currentViewMode = this.getProperty("ViewMode");
+        return `<div class="widget-content widget-ganttChart">
+                    <div class="gantt_container" width="${widgetWidth}" height="${widgetHeight}"/>
+                </div>`;
+    };
+
+    createRule(name,rules){
+        if(!this.elementStyleSheet) {
+            const style = document.createElement('style');
+            style.type = 'text/css';
+            document.getElementsByTagName('head')[0].appendChild(style);
+            this.elementStyleSheet = style.sheet as CSSStyleSheet;
+        }
+
+        if(!this.elementStyleSheet || !this.elementStyleSheet.insertRule)
+            this.elementStyleSheet.addRule(name, rules);
+        else
+            this.elementStyleSheet.insertRule(name+"{"+rules+"}",0);
+    }
+
+    afterRender() {
+        if(this.getProperty("AlwaysShowHandles")) {
+            this.createRule(".gantt_container .handle-group > rect", "opacity: 0.7; visibility: visible;");
+        }
+        if(this.getProperty("ShowBaselines")) {
+            this.createRule('.gantt_container .bar-progress', 'transform: translateY(-5px);');
+        }
+    }
+
+    updateProperty(info: TWUpdatePropertyInfo) {
+        if (info.TargetProperty === "Data") {
+            let rows = info.ActualDataRows;
+            let tooltips: string[] = [];
+            if (this.getProperty("TooltipField1")) {
+                tooltips.push(this.getProperty("TooltipField1"));
+            }
+            if (this.getProperty("TooltipField2")) {
+                tooltips.push(this.getProperty("TooltipField2"));
+            }
+            if (this.getProperty("TooltipField3")) {
+                tooltips.push(this.getProperty("TooltipField3"));
+            }
+            this.loadChart(rows, this.getProperty('TaskID'), this.getProperty('TaskName'), this.getProperty('StartDate'),
+                this.getProperty('EndDate'), this.getProperty('Relationships'), this.getProperty('Completed'), this.getProperty("CustomClass"), tooltips);
+        }
+        
+    }
+
+    handleSelectionUpdate(propertyName, selectedRows, selectedRowsIndices) {
+        if (propertyName == "Data") {
+            // TODO: implement selection
+        }
+    }
+
+    loadChart(rows, taskId, taskName, startDate, endDate, relationships, completed, customClass, tooltips: string[]) {
+        let data = [];
+        for (let i = 0; i < rows.length; i++) {
+            let row = rows[i];
+            data.push({
+                index: i,
+                id: row[taskId],
+                name: row[taskName],
+                start: row[startDate],
+                end: row[endDate],
+                progress: row[completed],
+                dependencies: row[relationships],
+                custom_class: row[customClass],
+                parentObj: row
+            });
+        }
+        console.log(data);
+
+        this.currentGanttChart = new Gantt(this.jqElement.find(".gantt_container")[0], data,
+            {
+                header_height: this.getProperty('HeaderHeight'),
+                column_width: this.getProperty('ColumnWidth'),
+                view_modes: ['Quarter Day', 'Half Day', 'Day', 'Week', 'Month'],
+                bar_height: this.getProperty('ItemHeight'),
+                bar_corner_radius: this.getProperty('BarCornerRadius'),
+                arrow_curve: this.getProperty("ArrowRadius"),
+                padding: this.getProperty('Padding'),
+                view_mode: this.currentViewMode,
+                date_format: TW.Runtime.convertLocalizableString(this.getProperty('DateFormat')),
+                custom_popup_html: (task) => {
+                    const end_date = task._end.toLocaleDateString("en-US");
+                    const contents = `${tooltips.map((tooltip) => {
+                        return `<p><strong>${tooltip}</strong>: ${task.parentObj[tooltip]}</p>`
+                    }).join("")}`;
+                    // console.log(contents);
+                    const percentageReport = task.progress > 100 ? `100% Completed, ${task.progress -100}% overtime!`: `${task.progress}% completed!`;
+                    // console.log(percentageReport);
+                    return `
+                    <div class="gantt-details-container" style="width: 200px">
+                        <div class="title">${task.name}</div>
+                        <div class="subtitle">
+                            ${contents}
+                            <p>${task.progress > 100 ? 'Finished on: ' : 'Expected to finish by: '}${end_date}</p>
+                            <p>${percentageReport}</p>
+                            
+                        </div>
+                    </div>
+                    `;
+
+                },
+                on_view_change: (mode) => {
+                    this.currentViewMode = mode;
+                    this.setProperty("ViewMode", mode);
+                    if (this.currentGanttChart) {
+                        this.drawNowBar(this.currentGanttChart);
+                    }
+                },
+                on_click: (task) => {
+                    this.updateSelection('Data', [task.index]);
+                },
+
+            });
+        this.drawNowBar(this.currentGanttChart);
+    };
+
+    drawNowBar(gantt: any) {
+        // add the current timestamp
+        const x =
+            (Date.now() - gantt.gantt_start.getTime()) / (1000 * 60 * 60 * gantt.options.step) * gantt.options.column_width;
+        const y = 0;
+
+        const width = 2;
+        const height =
+            (gantt.options.bar_height + gantt.options.padding) *
+            gantt.tasks.length +
+            gantt.options.header_height +
+            gantt.options.padding / 2;
+        console.log(x,height);
+        const nowRect = document.createElementNS('http://www.w3.org/2000/svg', "rect");
+        nowRect.setAttribute("x", Math.floor(x).toString());
+        nowRect.setAttribute("y", y.toString());
+        nowRect.setAttribute("class", "now-hightlight");
+        nowRect.setAttribute("width", width.toString());
+        nowRect.setAttribute("height", height.toString());
+        const nowRectTitle = document.createElementNS('http://www.w3.org/2000/svg', "title");
+        nowRectTitle.innerHTML = `Now: ${new Date()}`;
+        nowRect.appendChild(nowRectTitle);
+
+        gantt.layers.grid.appendChild(nowRect);
+    }
+    beforeDestroy?(): void {
+        // resetting current widget
+    }
+}
